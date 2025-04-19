8b2e15dddf53c984bc86d69008f8a925a5aa40f6
TayrineSoares <tayrinecristina@hotmail.com>
2025-04-19 12:09:57 -0400

    Add authentication and fetch the counts on dashboard controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
+
+
   def show
+    # Fetch the count of products and categories
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
