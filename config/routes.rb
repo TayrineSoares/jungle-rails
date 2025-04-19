98f0310a98b0fd1102cd40756554b7acfac3abc2
TayrineSoares <tayrinecristina@hotmail.com>
2025-04-19 12:57:21 -0400

    create about page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,10 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  # Add route for About page
+  get '/about', to: 'about#show'
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
