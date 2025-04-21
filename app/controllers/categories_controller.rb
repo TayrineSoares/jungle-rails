02fe4bccf24348def41437c5bf30036c5ceec343
TayrineSoares <tayrinecristina@hotmail.com>
2025-04-21 12:32:15 -0400

    generate a controller for admin/categories using rails generator

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,11 @@
+
 class CategoriesController < ApplicationController
 
   def show
+    # Find the category by the ID passed in the URL
     @category = Category.find(params[:id])
+
+    # Get all the products associated with this category, ordered by creation date
     @products = @category.products.order(created_at: :desc)
   end
 
