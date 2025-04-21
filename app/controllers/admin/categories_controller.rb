011cd55d909c033236acd5ff8adbc9858fd24402
TayrineSoares <tayrinecristina@hotmail.com>
2025-04-21 12:44:46 -0400

    update controller and add index and show html files in views/adm/categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,12 @@
 class Admin::CategoriesController < ApplicationController
+  # Display all categories
+  def index
+    @categories = Category.all
+  end
+
+  # Display a specific category and its products
+  def show
+    @category = Category.find(params[:id])
+    @products = @category.products.order(created_at: :desc)
+  end
 end
