09f702f6ebda45545885798fe35de235b25a535a
TayrineSoares <tayrinecristina@hotmail.com>
2025-04-21 13:01:47 -0400

    Update controller to receive new categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -9,4 +9,29 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
   end
+
+
+  # Prepare the form for creating a new category
+  def new
+    @category = Category.new
+  end
+
+  # Create a new category and save it to the database
+  def create 
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new, alert: 'There was an issue creating the category.'
+    end
+  end
+
+  private
+
+  # Strong parameters to allow only the name attribute for category creation
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
+
